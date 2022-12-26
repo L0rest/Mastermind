f96d4ad15f3cc59850fa81df627ac3df03404683
L0rest <jawad.maache@gmail.com>
2022-12-26 16:06:48 +0100

    Ajout boutons sur Frame

--- Vue.java
+++ Vue.java
@@ -1,5 +1,36 @@
 package ProjetJava.Mastermind;
 
-public class Vue {
-    
+import java.awt.*;
+import java.util.Observable;
+import java.util.Observer;
+
+public class Vue extends Frame implements Observer  {
+
+    Panel VueClavier;
+    Canvas VuePropositions;
+
+    public Vue() {
+        super();
+        this.VueClavier = new Panel();
+
+        for (int i = 0; i < Modele.COULEURS.length; i++) {
+            Button bouton = new Button();
+            bouton.setPreferredSize(new Dimension(35,35));
+            bouton.setBackground(Modele.COULEURS[i]);
+            this.VueClavier.add(bouton);
+        }
+
+        this.add(this.VueClavier);
+        this.pack();
+        this.setVisible(true);
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+
+    }
+
+    public static void main(String[] args) {
+        new Vue();
+    }
 }
