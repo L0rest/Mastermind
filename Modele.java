37317b42554d91d1527ab9cadfa3c9a838b3c641
L0rest <jawad.maache@gmail.com>
2022-12-26 10:45:04 +0100

    Ajout variables + Constructeur

--- Modele.java
+++ Modele.java
@@ -7,8 +7,21 @@ public class Modele {
     static final Color[] COULEURS = {Color.YELLOW, Color.GREEN, Color.BLUE, Color.MAGENTA, Color.RED, Color.ORANGE, Color.WHITE, Color.BLACK};
     static final int N_TENTATIVES = 10;
     static final int DIFFICULTE = 4;
-    public enum ETAT {
+    public enum Etat {
         EN_COURS, GAGNE, PERDU
     }
+
+    public Etat état;
+    public Rangee combinaison;
+    public Rangee[] propositions;
+    public int tentative;
+
+    public Modele() {
+        this.état = Etat.EN_COURS;
+        this.combinaison = new Rangee();
+        this.propositions = new Rangee[N_TENTATIVES];
+        this.tentative = 0;
+    }
+
 
 }
