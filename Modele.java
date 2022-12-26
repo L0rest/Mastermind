b7b5731e5ba1b6471b07296246a95d0af265366f
L0rest <jawad.maache@gmail.com>
2022-12-26 08:26:05 +0100

    Ajout constantes

--- Modele.java
+++ Modele.java
@@ -1,4 +1,15 @@
 package ProjetJava.Mastermind;
 
+import java.awt.*;
+
 public class Modele {
+
+    static final Color[] COULEURS = {
+        Color.YELLOW, Color.GREEN, Color.BLUE, Color.MAGENTA, Color.RED, Color.ORANGE, Color.WHITE, Color.BLACK
+    };
+    static final int N_TENTATIVES = 10;
+    static final int DIFFICULTE = 4;
+    public enum ETAT {
+        EN_COURS, GAGNE, PERDU
+    }
 }
