03c20f1f1d6a211bdf7c0b37411f9a7d8faed0ab
L0rest <jawad.maache@gmail.com>
2022-12-27 09:05:51 +0100

    Ajout Canvas sur Frame

--- Vue.java
+++ Vue.java
@@ -8,19 +8,28 @@ public class Vue extends Frame implements Observer  {
 
     Panel VueClavier;
     Canvas VuePropositions;
+    Modele modl;
 
-    public Vue() {
+    public Vue(Modele m) {
         super();
-        this.VueClavier = new Panel();
+        this.modl = m;
 
+        this.setLayout(new BorderLayout());
+
+        this.VueClavier = new Panel();
+        this.VueClavier.setLayout(new FlowLayout());
         for (int i = 0; i < Modele.COULEURS.length; i++) {
             Button bouton = new Button();
             bouton.setPreferredSize(new Dimension(35,35));
             bouton.setBackground(Modele.COULEURS[i]);
             this.VueClavier.add(bouton);
         }
+        this.add(this.VueClavier, BorderLayout.SOUTH);
 
-        this.add(this.VueClavier);
+        this.VuePropositions = new Canvas();
+        this.VuePropositions.setPreferredSize(new Dimension(Modele.DIFFICULTE*80 + 40,Modele.N_TENTATIVES*50));
+        this.add(this.VuePropositions, BorderLayout.NORTH);
+
         this.pack();
         this.setVisible(true);
     }
