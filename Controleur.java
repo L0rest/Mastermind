fb46f622280138914cd7f30984d208761cbde421
L0rest <jawad.maache@gmail.com>
2022-12-28 08:36:54 +0100

    Ajout constructeur

--- Controleur.java
+++ Controleur.java
@@ -1,4 +1,18 @@
 package ProjetJava.Mastermind;
 
-public class Controleur {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Controleur implements ActionListener {
+
+    Modele modl;
+
+    public Controleur(Modele m) {
+        this.modl = m;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
