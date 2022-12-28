e5f02aae3078ca40a393c6e5576b914636d3054c
L0rest <jawad.maache@gmail.com>
2022-12-28 08:18:04 +0100

    Ajout méthode paint

--- VuePropositions.java
+++ VuePropositions.java
@@ -14,6 +14,15 @@ public class VuePropositions extends Canvas implements Observer {
         this.setPreferredSize(new Dimension(Modele.DIFFICULTE*80 + 40,Modele.N_TENTATIVES*50));
     }
 
+    public void paint(Graphics g) {
+        do {
+            for (int i = 0; i < Modele.DIFFICULTE; i++) {
+                g.drawOval((60*i) + 30, 10, 40, 40);
+            }
+        } while (this.modl.propositions[this.modl.tentative] != null);
+
+    }
+
     @Override
     public void update(Observable o, Object arg) {
 
