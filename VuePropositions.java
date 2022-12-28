092f73dccada541248dd901b1273ee1db38c240b
L0rest <jawad.maache@gmail.com>
2022-12-28 13:49:14 +0100

    Ajout observeur + méthode paint

--- VuePropositions.java
+++ VuePropositions.java
@@ -12,19 +12,27 @@ public class VuePropositions extends Canvas implements Observer {
         super();
         this.modl = m;
         this.setPreferredSize(new Dimension(Modele.DIFFICULTE*80 + 40,Modele.N_TENTATIVES*50));
+
+        this.modl.addObserver(this);
+
     }
 
     public void paint(Graphics g) {
-        do {
-            for (int i = 0; i < Modele.DIFFICULTE; i++) {
-                g.drawOval((60*i) + 30, 10, 40, 40);
+        for (int i = 0; i < this.modl.propositions.length; i++) {
+            if (this.modl.propositions[i] != null) {
+                for (int j = 0; j < Modele.DIFFICULTE; j++) {
+                    g.setColor(this.modl.propositions[i].jetons[j]);
+                    g.drawOval((60*j) + 30, (40*i) + 10, 40, 40);
+                }
+                i++;
             }
-        } while (this.modl.propositions[this.modl.tentative] != null);
+        }
+
 
     }
 
     @Override
     public void update(Observable o, Object arg) {
-
+        repaint();
     }
 }
