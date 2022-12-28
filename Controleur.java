cb3edf9763443d83ede75cabbd8f6c23d39fd430
L0rest <jawad.maache@gmail.com>
2022-12-28 13:50:28 +0100

    Ajout constructeur

--- Controleur.java
+++ Controleur.java
@@ -2,17 +2,26 @@ package ProjetJava.Mastermind;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 public class Controleur implements ActionListener {
 
     Modele modl;
+    Random rand;
 
     public Controleur(Modele m) {
         this.modl = m;
+        this.rand = new Random();
+        for (int i = 0; i < Modele.DIFFICULTE; i++) {
+            this.modl.combinaison.jetons[i] = Modele.COULEURS[rand.nextInt(Modele.COULEURS.length)];
+        }
+        this.modl.propositions[0] = new Rangee();
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        if (this.modl.état == Modele.Etat.EN_COURS) {
+            this.modl.completer_prop();
+        }
     }
 }
