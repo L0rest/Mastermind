b4729ca19687cdf071bff286a4746a5e5bce3efa
L0rest <jawad.maache@gmail.com>
2022-12-29 07:26:02 +0100

    Ajout message fin de partie + ajout commande des boutons

--- VueClavier.java
+++ VueClavier.java
@@ -16,10 +16,12 @@ public class VueClavier extends Panel implements Observer {
         this.modl = m;
 
         this.setLayout(new FlowLayout());
+        this.setPreferredSize(new Dimension(Modele.DIFFICULTE*80 + 40, 50));
         for (int i = 0; i < Modele.COULEURS.length; i++) {
             Button bouton = new Button();
             bouton.setPreferredSize(new Dimension(35,35));
             bouton.setBackground(Modele.COULEURS[i]);
+            bouton.setActionCommand(String.valueOf(Modele.COULEURS[i].getRGB()));
             this.listeboutons[i] = bouton;
             this.listeboutons[i].addActionListener(l);
             this.add(bouton);
@@ -30,6 +32,15 @@ public class VueClavier extends Panel implements Observer {
 
     @Override
     public void update(Observable o, Object arg) {
-
+        if (this.modl.état == Modele.Etat.GAGNE) {
+            this.removeAll();
+            this.add(new Label("Gagné"));
+            repaint();
+        }
+        else if (this.modl.état == Modele.Etat.PERDU) {
+            this.removeAll();
+            this.add(new Label("Perdu"));
+            repaint();
+        }
     }
 }
