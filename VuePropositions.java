86621a6c01e4e3dc7e07643fa93ed16efbe74b82
L0rest <jawad.maache@gmail.com>
2022-12-29 07:26:43 +0100

    Affichage combinaisons couleur au fil de la partie

--- VuePropositions.java
+++ VuePropositions.java
@@ -11,7 +11,7 @@ public class VuePropositions extends Canvas implements Observer {
     public VuePropositions(Modele m) {
         super();
         this.modl = m;
-        this.setPreferredSize(new Dimension(Modele.DIFFICULTE*80 + 40,Modele.N_TENTATIVES*50));
+        this.setPreferredSize(new Dimension(Modele.DIFFICULTE*80 + 40,Modele.N_TENTATIVES*80));
 
         this.modl.addObserver(this);
 
@@ -21,10 +21,13 @@ public class VuePropositions extends Canvas implements Observer {
         for (int i = 0; i < this.modl.propositions.length; i++) {
             if (this.modl.propositions[i] != null) {
                 for (int j = 0; j < Modele.DIFFICULTE; j++) {
-                    g.setColor(this.modl.propositions[i].jetons[j]);
-                    g.drawOval((60*j) + 30, (40*i) + 10, 40, 40);
+                    if (this.modl.propositions[i].jetons[j] != null) {
+                        g.setColor(this.modl.propositions[i].jetons[j]);
+                        g.fillOval((60*j) + 30, (60*i) + 10, 40, 40);
+                    }
+                    g.setColor(Color.BLACK);
+                    g.drawOval((60*j) + 30, (60*i) + 10, 40, 40);
                 }
-                i++;
             }
         }
 
